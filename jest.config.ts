ee7e7a985f66d3165e83fa188c257129aeca10c1
wallace-sf <wallaceedua@gmail.com>
2023-03-13 16:52:58 -0300

    chore: jest test regex

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@ export default {
   preset: 'ts-jest',
   roots: ['<rootDir>/__tests__'],
   testEnvironment: 'jsdom',
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  testRegex: 'WorkflowManager.test.tsx',
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
